a0daa1d0b793e6ec657c58c3bba5a2ff49403ca7
tsneemahmad <tsneemamarat@gmail.com>
2020-02-20 20:41:13 +0200

    changes on stle

--- js/app.js
+++ js/app.js
@@ -8,10 +8,8 @@ var know = prompt("do you know me personally?").toLowerCase();
 console.log(know);
 if (know === 'yes' || know === 'y') {
     mark = mark + 1;
-    // console.log (know);
     alert('your answer is right');
 } else if (know === 'no' || know === 'n') {
-    // console.log (know);
     alert('your answer is right');
 } else {
     alert('try again with yes or no');
@@ -22,10 +20,8 @@ var know2 = prompt("do you think i love sport?").toLowerCase();
 console.log(know2);
 if (know2 === 'yes' || know2 === 'y') {
     mark= mark+1;
-    // console.log (know2);
     alert('bingo, im addicted to sport');
 } else if (know2 === 'no' || know2 === 'n') {
-    // console.log (know2);
     alert('Oops!, im addicted to sport');
 } else {
     alert('try again with yes or no');
@@ -35,10 +31,8 @@ var know3 = prompt("I love my speciality?").toLowerCase();
 console.log(know3);
 if (know3 === 'yes' || know3 === 'y') {
     mark= mark + 1;
-    // console.log (know3);
     alert('your answer is right');
 } else if (know3 === 'no' || know3 === 'n') {
-    // console.log (know3);
     alert('wrong, i do love my speciality');
 } else {
     alert('try again with yes or no');
@@ -48,10 +42,8 @@ var know4 = prompt("am i a tea person?").toLowerCase();
 console.log(know4);
 if (know4 === 'yes' || know4 === 'y') {
     mark= mark + 1;
-    // console.log (know4);
     alert('no im a coffee person');
 } else if (know4 === 'no' || know4 === 'n') {
-    // console.log (know4);
     alert('your answer is right');
 } else {
     alert('try again with yes or no');
@@ -61,10 +53,8 @@ var know5 = prompt("i like daisy?").toLowerCase();
 console.log(know5);
 if (know5 === 'yes' || know5 === 'y') {
     mark= mark + 1;
-    // console.log (know5);
     alert('your answer is right');
 } else if (know5 === 'no' || know5 === 'n') {
-    // console.log (know5);
     alert('you are wrong, im in love with daisy');
 } else {
     alert('try again with yes or no');
